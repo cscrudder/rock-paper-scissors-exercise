2fe58e21d96bff4d940bac8d5a162e1f0dac1576
cscrudder <cms447@georgetown.edu>
2022-01-26 21:10:47 -0700

    First draft of game code

--- game.py
+++ game.py
@@ -1,8 +1,65 @@
 # RPS Game by Colton Scrudder
 
+# Objectives:
+#https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 
-#
-# todo: write some Python code here to satisfy the exercise objectives
-# ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
-#
-print("RPS Shoot!")
+from random import choice
+
+# Print Welcome Message
+print("------------")
+print("Welcome 'Player 1' to my Rock-Paper-Scissors game...")
+print("------------")
+
+# Processing User Inputs
+user_move = input("Please choose 'rock', 'paper', or 'scissors': ")
+
+# Validating User Inputs
+if user_move.lower() == 'rock':
+    user_move = 'rock'
+elif user_move.lower() == 'paper':
+    user_move = 'paper'
+elif user_move.lower() == 'scissors':
+    user_move = 'scissors'
+else:
+    print("I did not recognize that input. Please play again!")
+    exit()
+
+# Simulating Computer Selection
+options = ['rock','paper','scissors']
+computer_move = choice(options)
+
+# Determining the Winner
+if computer_move == 'rock':
+    if user_move == "rock":
+        result = 'tie'
+    elif user_move == 'paper':
+        result = 'win'
+    elif user_move == 'scissors':
+        result = "loss"
+elif computer_move == 'paper':
+    if user_move == "rock":
+        result = "loss"
+    elif user_move == 'paper':
+        result = 'tie'
+    elif user_move == 'scissors':
+        result = 'win'
+elif computer_move == 'scissors':
+    if user_move == "rock":
+        result = 'win'
+    elif user_move == 'paper':
+        result = "loss"
+    elif user_move == 'scissors':
+        result = 'tie'
+
+#Displaying Results
+print("You chose:", user_move)
+print("The computer chose:", computer_move)
+print("------------")
+if result == 'win':
+    print('You won! Good work.')
+elif result == 'loss':
+    print("Oh, the computer won. It's okay.")
+else:
+    print("You tied. You're just as good as a computer!")
+print("------------")
+print("Thanks for playing. Please play again.")
